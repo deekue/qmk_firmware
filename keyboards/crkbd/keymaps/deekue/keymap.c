61b972765981cfa0d694f814fa6761f48669cc6b
DQ <daniel@chaosengine.net>
2021-02-27 12:08:53 -0800

    remove RESET from thumb layers
    accidentally triggering it fairly often
    not really needed given I have the hardware reset button

--- keyboards/crkbd/keymaps/deekue/keymap.c
+++ keyboards/crkbd/keymaps/deekue/keymap.c
@@ -78,25 +78,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 //#if defined MIRYOKU_NAV_VI
   [NAVR] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    C(KC_Y), C(KC_V), C(KC_C), C(KC_X), C(KC_Z),
+    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    C(KC_Y), C(KC_V), C(KC_C), C(KC_X), C(KC_Z),
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, S(KC_CAPS),
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_INS,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_ENT,  KC_SPC,  KC_TAB,  U_NP,    U_NP
   ),
   [NAVR_MAC] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    SCMD(KC_C), LCMD(KC_V), LCMD(KC_C), LCMD(KC_X), LCMD(KC_Z),
+    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    SCMD(KC_C), LCMD(KC_V), LCMD(KC_C), LCMD(KC_X), LCMD(KC_Z),
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_LEFT,    KC_DOWN, KC_UP,   KC_RGHT, KC_CAPS,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    KC_HOME,    KC_PGDN, KC_PGUP, KC_END,  KC_INS,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_ENT,     KC_SPC,  KC_TAB,  U_NP,    U_NP
   ),
   [MOUR] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
+    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, U_NU,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, U_NU,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_BTN1, KC_BTN3, KC_BTN2, U_NP,    U_NP
   ),
   [MEDR] = LAYOUT_miryoku(
-    RESET,   U_NA,    U_NA,    U_NA,    U_NA,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
+    U_NA,    U_NA,    U_NA,    U_NA,    U_NA,    RGB_TOG, RGB_MOD, RGB_HUI, RGB_SAI, RGB_VAI,
     KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, U_NA,    KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, U_NU,
     U_NA,    KC_ALGR, U_NA,    U_NA,    U_NA,    U_NU,    U_NU,    U_NU,    U_NU,    U_NU,
     U_NP,    U_NP,    U_NA,    U_NA,    U_NA,    KC_MSTP, KC_MPLY, KC_MUTE, U_NP,    U_NP
@@ -110,19 +110,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
 #endif
   [FUNL] = LAYOUT_miryoku(
-    KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,     U_NA, U_NA,    U_NA,    U_NA,    RESET,
+    KC_F12,  KC_F7,   KC_F8,   KC_F9,   KC_PSCR,     U_NA, U_NA,    U_NA,    U_NA,    U_NA,
     KC_F11,  KC_F4,   KC_F5,   KC_F6,   KC_SLCK,     U_NA, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
     KC_F10,  KC_F1,   KC_F2,   KC_F3,   KC_PAUS,     U_NA, U_NA,    U_NA,    KC_ALGR, U_NA,
     U_NP,    U_NP,    KC_APP,  KC_BSPC, KC_DEL,      U_NA, U_NA,    U_NA,    U_NP,    U_NP
   ),
   [NSL] = LAYOUT_miryoku(
-    KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,     U_NA, U_NA,    U_NA,    U_NA,    RESET,
+    KC_LBRC, KC_7,    KC_8,    KC_9,    KC_RBRC,     U_NA, U_NA,    U_NA,    U_NA,    U_NA,
     KC_SCLN, KC_4,    KC_5,    KC_6,    KC_EQL,      U_NA, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
     KC_GRV,  KC_1,    KC_2,    KC_3,    KC_BSLS,     U_NA, U_NA,    U_NA,    KC_ALGR, U_NA,
     U_NP,    U_NP,    KC_DOT,  KC_0,    KC_MINS,     U_NA, U_NA,    U_NA,    U_NP,    U_NP
   ),
   [NSSL] = LAYOUT_miryoku(
-    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,     U_NA, U_NA,    U_NA,    U_NA,    RESET,
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,     U_NA, U_NA,    U_NA,    U_NA,    U_NA,
     KC_COLN, KC_DLR,  KC_PERC, KC_CIRC, KC_PLUS,     U_NA, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI,
     KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_PIPE,     U_NA, U_NA,    U_NA,    KC_ALGR, U_NA,
     U_NP,    U_NP,    KC_LPRN, KC_RPRN, KC_UNDS,     U_NA, U_NA,    U_NA,    U_NP,    U_NP
